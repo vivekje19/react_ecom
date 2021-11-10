66aab16bfc0e2ab3b227dfc7c0e25e294138e447
Vivek Ranjan Pandey <vivekranjan.varanasi@gmail.com>
2021-11-10 13:06:50 +0530

    add router concept

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
+import { Routes, Route} from 'react-router-dom';
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 function App() {
   return (
-    <div><HomePage /></div>
+    <div>
+      
+        <Routes>
+          <Route  path="/" element = {<HomePage />} />
+          <Route  path="/hats" element = {<HatsPage />} />
+        </Routes>
+        
+      
+    </div>
   );
 }
 
