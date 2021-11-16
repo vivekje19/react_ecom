dbfadfd44774e0bb0ebbf29c5ce5d9bca5eab35b
Vivek Ranjan Pandey <vivekranjan.varanasi@gmail.com>
2021-11-16 10:24:27 +0530

    implemented firebase utils, inculuded ability to store users information into database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor(){
@@ -17,9 +17,20 @@ class App extends React.Component {
   } 
   unsubscribeFromAuth = null;
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user});
-      console.log(user); 
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser:{
+              id:snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      }
+      
+    
     })
   }
 
--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './sign-in-sign-up.styles.scss';
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInSignUpPage = ()=> (
     <div className='sign-in-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 );
 export default SignInSignUpPage;
